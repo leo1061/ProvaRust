7c6a9d315d72c668203e72f95cc0a1c8a7c61df5
leo1061 <leonardoventura.it@gmail.com>
2025-02-21 12:32:55 +0100

    Scritta Ciao Mondo

---
+++ shamir_SS/target/debug/deps/shamir_SS-8895aa1b26788714.d
@@ -0,0 +1,5 @@
+C:\Users\leona\Documents\ProvaRust\shamir_SS\target\debug\deps\libshamir_SS-8895aa1b26788714.rmeta: src\main.rs
+
+C:\Users\leona\Documents\ProvaRust\shamir_SS\target\debug\deps\shamir_SS-8895aa1b26788714.d: src\main.rs
+
+src\main.rs:
