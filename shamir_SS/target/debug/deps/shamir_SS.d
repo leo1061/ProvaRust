8c4d1610382eb590ce4fb22ffb3cc9a9c6f4389c
leo1061 <leonardoventura.it@gmail.com>
2025-02-21 12:29:24 +0100

    Creazione della cartella shamir_SS

---
+++ shamir_SS/target/debug/deps/shamir_SS.d
@@ -0,0 +1,5 @@
+C:\Users\leona\Documents\ProvaRust\shamir_SS\target\debug\deps\shamir_SS.exe: src\main.rs
+
+C:\Users\leona\Documents\ProvaRust\shamir_SS\target\debug\deps\shamir_SS.d: src\main.rs
+
+src\main.rs:
---
+++ shamir_SS/target/debug/shamir_SS.d
@@ -0,0 +1,1 @@
+C:\Users\leona\Documents\ProvaRust\shamir_SS\target\debug\shamir_SS.exe: C:\Users\leona\Documents\ProvaRust\shamir_SS\src\main.rs
